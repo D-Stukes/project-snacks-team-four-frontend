568fdbcec199d3f9b4fc125c689c89badd78f2d5
D-Stukes <dstukes888@gmail.com>
2023-02-01 12:12:49 -0500

    updated fonts, colors, font styles and button specs on Index page

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,21 +3,24 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="nav">
-      <h1>
-        <Link className="navButton" to="/">
-          Snacks
+      <h1  >
+        <Link to="/" className="navButtonHdg">
+         SNACKS LOG {" "}
         </Link>
       </h1>
-      <button>
-        <Link className="navButton" to="/snacks/new">
-          New Snack
-        </Link>
-      </button>
-      <button>
-        <Link className="navButton" to="/snacks">
+      
+      <button className="outerNavButton1">
+      <Link className="navButton" to="/snacks">
           {" "}
           Snack List{" "}
         </Link>
+      </button>
+
+      <button className="outerNavButton2">
+      <Link className="navButton" to="/snacks/new">
+          New Snack
+        </Link>
+    
       </button>
     </nav>
   );
